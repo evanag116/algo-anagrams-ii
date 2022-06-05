a0adbaea55686c10c5f20cb2a94829b0ea27542b
Evan Garcia <evanag116@yahoo.com>
2022-06-05 16:56:31 -0500

    completed in js

--- js/anagram2.js
+++ js/anagram2.js
@@ -1,3 +1,24 @@
 exports.anagramsFor = function(word, listOfWords) {
 
+    let answer = []
+
+    word = word.toLowerCase().replace(/ /g, "")
+    word = word.split("")
+    word = word.sort()
+    word = word.join("")
+
+    for (let i = 0; i < listOfWords.length; i++) {
+        let testWord = listOfWords[i].toLowerCase().replace(/ /g, "")
+        testWord = testWord.split("")
+        testWord = testWord.sort()
+        testWord = testWord.join("")
+
+        if (word === testWord)
+        answer.push(listOfWords[i])
+    }
+
+    
+    return answer
+
 };
+
